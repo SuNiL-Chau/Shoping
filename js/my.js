34e0db4a342de7f0d3297d9d099073f98c0bfc4f
Sunil Chaudhary <46418190+SuNiL-Chau@users.noreply.github.com>
2019-09-08 23:58:10 +0530

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,17 @@
+function linkedin() {
+    $(".linkedin").click(function() {
+        window.open("https://www.linkedin.com", "_blank");
+    });
+}
+
+function twitter() {
+    $(".twitter").click(function() {
+        window.open("https://www.twitter.com", "_blank");
+    });
+}
+
+function facebook() {
+    $(".facebook").click(function() {
+        window.open("https://www.facebook.com", "_blank");
+    });
+}
